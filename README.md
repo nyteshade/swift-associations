8f1eff8a38797ed87fddade860ce884686fb1144
Brielle Harrison <nyteshade@gmail.com>
2024-04-07 13:10:23 -0600

    Updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# swift-associations
-Allows an easy way to associate one value with another in Swift
+# Associable
+
+Associable is a Swift package that provides a protocol for associating values with class instances using the Objective-C runtime. This allows you to add custom data to any class instance without modifying the class definition itself.
+
+## Features
+
+- Associate values with class instances using a key
+- Retrieve associated values using a key
+- Check if a value is associated with a key
+- Disassociate values for a specific key
+- Disassociate all associated values
+
+## Installation
+
+### Swift Package Manager
+
+To install Associable using Swift Package Manager, add the following line to your `Package.swift` file:
+
+```swift
+dependencies: [
+    .package(url: "https://github.com/your-username/Associable.git", from: "1.0.0")
+]
+```
+
+## Usage
+
+1. Import the Associable package in your Swift file:
+
+```swift
+import Associable
+```
+
+2. Make your class conform to the `Associable` protocol:
+
+```swift
+class MyClass: Associable {}
+```
+
+3. Associate a value with an instance of your class:
+
+```swift
+let instance = MyClass()
+instance.associate("Hello, World!", forKey: "greeting")
+```
+
+4. Retrieve the associated value:
+
+```swift
+let greeting: String? = instance.associatedValue(forKey: "greeting")
+print(greeting) // Outputs "Hello, World!"
+```
+
+5. Check if a value is associated with a key:
+
+```swift
+if instance.isAssociated(withKey: "greeting") {
+    print("A greeting is associated")
+} else {
+    print("No greeting is associated")
+}
+```
+
+6. Disassociate a value for a specific key:
+
+```swift
+instance.disassociate(forKey: "greeting")
+```
+
+7. Disassociate all associated values:
+
+```swift
+instance.disassociateAll()
+
+// also for any class that is Associable
+deinit {
+  disassociateAll()
+}
+```
+
+## License
+
+Associable is released under the MIT License.
--- Sources/Associations/Associable.swift
+++ Sources/Associations/Associable.swift
@@ -7,26 +7,63 @@
 
 import Foundation
 
-/// Protocol that enables association of values with class instances.
+/// A protocol that enables the association of values with class instances. This can be used to
+/// add custom data to any class instance without modifying the class definition itself.
 public protocol Associable: AnyObject { }
 
 public extension Associable {
-  /// Associates a value with the current instance using a string key.
+  /// Associates a value with this instance using a specified key. The value is retained
+  /// non-atomically with the instance.
+  ///
+  /// - Parameters:
+  ///   - value: The value to associate with this instance.
+  ///   - key: The key under which to store the value.
+  ///
+  /// Example:
+  /// ```
+  /// class MyClass: Associable {}
+  /// let instance = MyClass()
+  /// instance.associate("Hello, World!", forKey: "greeting")
+  /// ```
   func associate<T>(_ value: T, forKey key: String) {
     let keyPointer = UnsafeRawPointer(bitPattern: key.hashValue)!
     objc_setAssociatedObject(self, keyPointer, value, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
   }
   
-  /// Retrieves the associated value for the current instance using a string key.
+  /// Retrieves the value associated with this instance for a specified key.
+  ///
+  /// - Parameter key: The key for the associated value.
+  /// - Returns: The value associated with the key, or nil if no value is associated.
+  ///
+  /// Example:
+  /// ```
+  /// class MyClass: Associable {}
+  /// let instance = MyClass()
+  /// instance.associate("Hello, World!", forKey: "greeting")
+  /// let greeting: String? = instance.associatedValue(forKey: "greeting")
+  /// print(greeting) // Outputs "Hello, World!"
+  /// ```
   func associatedValue<T>(forKey key: String) -> T? {
     let keyPointer = UnsafeRawPointer(bitPattern: key.hashValue)!
     return objc_getAssociatedObject(self, keyPointer) as? T
   }
   
-  /// Checks to see if a non-nil value is associated with the object and the supplied key
+  /// Checks whether a non-nil value is associated with this instance for the specified key.
   ///
-  /// - Parameter withKey: the key used to check for an associated value
-  /// - 
+  /// - Parameter key: The key used to check for an associated value.
+  /// - Returns: `true` if there is a value associated with the key, `false` otherwise.
+  ///
+  /// Example:
+  /// ```
+  /// class MyClass: Associable {}
+  /// let instance = MyClass()
+  /// instance.associate("Hello, World!", forKey: "greeting")
+  /// if instance.isAssociated(withKey: "greeting") {
+  ///   print("A greeting is associated")
+  /// } else {
+  ///   print("No greeting is associated")
+  /// }
+  /// ```
   func isAssociated(withKey key:String) -> Bool {
     let keyPointer = UnsafeRawPointer(bitPattern: key.hashValue)!
     let value: Any? = objc_getAssociatedObject(self, keyPointer)
@@ -34,13 +71,32 @@ public extension Associable {
     return value != nil
   }
   
-  /// Disassociates the value for the current instance for a given key.
+  /// Disassociates the value for this instance for the specified key.
+  ///
+  /// - Parameter key: The key for which the association should be removed.
+  ///
+  /// Example:
+  /// ```
+  /// class MyClass: Associable {}
+  /// let instance = MyClass()
+  /// instance.associate("Hello, World!", forKey: "greeting")
+  /// instance.disassociate(forKey: "greeting")
+  /// ```
   func disassociate(forKey key: String) {
     let keyPointer = UnsafeRawPointer(bitPattern: key.hashValue)!
     objc_setAssociatedObject(self, keyPointer, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
   }
   
-  /// Disassociates all values for the current instance.
+  /// Disassociates all values associated with this instance.
+  ///
+  /// Example:
+  /// ```
+  /// class MyClass: Associable {}
+  /// let instance = MyClass()
+  /// instance.associate("Hello, World!", forKey: "greeting")
+  /// instance.associate(42, forKey: "number")
+  /// instance.disassociateAll()
+  /// ```
   func disassociateAll() {
     objc_removeAssociatedObjects(self)
   }
